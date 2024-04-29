8fd93e75b3b85056977416119bd0c43ed237f8a1
beworthK <beworth.k@gmail.com>
2024-04-29 13:15:02 +0900

    Part 1. 스프링 개발 환경 구축

---
+++ spring-mvc-code/ex00/src/test/java/org/zerock/persistence/JDBCTests.java
@@ -0,0 +1,44 @@
+package org.zerock.persistence;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class JDBCTests {
+	
+	/** JDBC 테스트 코드
+	 * JDBC 드라이버가 정상적으로 추가되었고, 데이터베이스의 연결이 가능한지 확인하는 테스트 코드
+	 */
+	
+	static {
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testConnection() {
+		
+		try(Connection con = DriverManager.getConnection(
+				"jdbc:mariadb://localhost:3306/book" //url
+				, "book_ex"
+				, "book_ex")) {
+			
+			log.info(con);
+		}catch (Exception e) {
+			fail(e.getMessage());
+		}
+		
+		
+	}
+	
+
+}
